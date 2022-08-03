8be55e75c8c9d452cb3b09a5a1a8cb112979eea4
gtmnayan <gtmnayan@gmail.com>
2022-08-03 12:57:21 +0545

    eliminate unused code

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,6 @@ use vis_core::analyzer;
 pub struct VisInfo {
     beat: u64,
     beat_volume: f32,
-    volume: f32,
-    analyzer: analyzer::FourierAnalyzer,
-    spectrum: analyzer::Spectrum<Vec<f32>>,
 }
 
 fn main() {
@@ -17,21 +14,16 @@ fn main() {
     vis_core::default_log();
 
     let mut keyboard = get_keyboard(Arc::new(AtomicBool::new(false))).unwrap();
-
+    keyboard.set_brightness(2);
     let mut frames = {
         // Analyzer {{{
         let mut beat = analyzer::BeatBuilder::new().build();
         let mut beat_num = 0;
 
-        let analyzer = analyzer::FourierBuilder::new().plan();
-
         vis_core::Visualizer::new(
             VisInfo {
                 beat: 0,
                 beat_volume: 0.0,
-                volume: 0.0,
-                spectrum: analyzer::Spectrum::new(vec![0.0; analyzer.buckets()], 0.0, 1.0),
-                analyzer,
             },
             move |info, samples| {
                 if beat.detect(&samples) {
@@ -39,10 +31,6 @@ fn main() {
                 }
                 info.beat = beat_num;
                 info.beat_volume = beat.last_volume();
-                info.volume = samples.volume(0.3);
-
-                info.analyzer.analyze(&samples);
-                info.spectrum.fill_from(&info.analyzer.average());
 
                 info
             },
@@ -52,10 +40,7 @@ fn main() {
         // }}}
     };
 
-    let frame_time =
-        std::time::Duration::from_micros(1000000 / vis_core::CONFIG.get_or("rgb.fps", 30));
-
-    let mut previous_time = 0.0;
+    let frame_time = std::time::Duration::from_micros(1000000 / 30);
 
     let mut last_beat = -100.0;
 
@@ -64,33 +49,29 @@ fn main() {
 
     for frame in frames.iter() {
         let start = std::time::Instant::now();
-        let delta = frame.time - previous_time;
 
-        let (volume, /* maxima, */ /* notes_rolling_spectrum, */ base_volume) =
-            frame.info(|info| {
-                if info.beat != last_beat_num {
-                    last_beat = frame.time;
-                    last_beat_num = info.beat;
-                }
-                let blue = (beat_rolling * 25.0) as u8;
-                let green = blue / 2;
+        let base_volume = frame.info(|info| {
+            if info.beat != last_beat_num {
+                last_beat = frame.time;
+                last_beat_num = info.beat;
+            }
+            let blue = (beat_rolling * 25.0) as u8;
+            let green = blue / 2;
 
-                let (a, b) = if last_beat_num.rem_euclid(2) == 0 {
-                    (1, 2)
-                } else {
-                    (2, 1)
-                };
-                keyboard.set_zone_by_index(a, [blue, 0, 0]);
-                keyboard.set_zone_by_index(b, [0, blue, 0]);
-                keyboard.set_zone_by_index(0, [0, green, blue]);
-                keyboard.set_zone_by_index(3, [0, green, blue]);
-                keyboard.refresh();
-                (info.volume, info.beat_volume)
-            });
+            let (a, b) = if last_beat_num.rem_euclid(2) == 0 {
+                (1, 2)
+            } else {
+                (2, 1)
+            };
+            keyboard.set_zone_by_index(a, [blue, 0, 0]);
+            keyboard.set_zone_by_index(b, [0, blue, 0]);
+            keyboard.set_zone_by_index(0, [0, green, blue]);
+            keyboard.set_zone_by_index(3, [0, green, blue]);
+            keyboard.refresh();
+            info.beat_volume
+        });
 
         beat_rolling = (beat_rolling * 0.95f32).max(base_volume);
-
-        previous_time = frame.time;
 
         let end = std::time::Instant::now();
         let dur = end - start;
