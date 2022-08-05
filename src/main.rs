23231a754dd182cb24782fbda5049f6a272e3dec
gtmnayan <gtmnayan@gmail.com>
2022-08-05 11:17:09 +0545

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+#![feature(duration_constants)]
+#![feature(stmt_expr_attributes)]
+
 use std::{
-    ops::BitXor,
-    sync::{atomic::AtomicBool, Arc},
+	ops::BitXor,
+	sync::{atomic::AtomicBool, Arc},
+	time::{Duration, Instant},
 };
 
 use lenovo_legion_hid::get_keyboard;
@@ -8,81 +12,77 @@ use vis_core::analyzer;
 
 #[derive(Debug, Clone)]
 pub struct VisInfo {
-    beat: u64,
-    beat_volume: f32,
+	beat: u64,
+	beat_volume: f32,
 }
 
 fn main() {
-    vis_core::default_config();
-    vis_core::default_log();
+	vis_core::default_config();
+	vis_core::default_log();
 
-    let mut keyboard = get_keyboard(Arc::new(AtomicBool::new(false))).unwrap();
-    keyboard.set_brightness(2);
-    let mut frames = {
-        // Analyzer {{{
-        let mut beat = analyzer::BeatBuilder::new().build();
-        let mut beat_num = 0;
+	let mut keyboard = get_keyboard(Arc::new(AtomicBool::new(false))).unwrap();
+	keyboard.set_brightness(2);
 
-        vis_core::Visualizer::new(
-            VisInfo {
-                beat: 0,
-                beat_volume: 0.0,
-            },
-            move |info, samples| {
-                if beat.detect(&samples) {
-                    beat_num += 1;
-                }
-                info.beat = beat_num;
-                info.beat_volume = beat.last_volume();
+	let mut frames = {
+		let mut beat = analyzer::BeatBuilder::new().build();
+		let mut beat_num = 0;
 
-                info
-            },
-        )
-        .async_analyzer(300)
-        .frames()
-        // }}}
-    };
+		vis_core::Visualizer::new(
+			VisInfo {
+				beat: 0,
+				beat_volume: 0.0,
+			},
+			move |info, samples| {
+				if beat.detect(&samples) {
+					beat_num += 1;
+				}
+				info.beat = beat_num;
+				info.beat_volume = beat.last_volume();
 
-    let frame_time = std::time::Duration::from_micros(1000000 / 30);
+				info
+			},
+		)
+		.async_analyzer(300)
+		.frames()
+	};
 
-    let mut beat_rolling = 0.0;
-    let mut last_beat_num = 0;
+	let frame_time = Duration::SECOND / 30;
 
-    for frame in frames.iter() {
-        let start = std::time::Instant::now();
+	let mut beat_rolling = 0.0;
+	let mut last_beat_num = 0;
 
-        let base_volume = frame.info(|info| {
-            if info.beat != last_beat_num {
-                last_beat_num = info.beat;
-            }
+	for frame in frames.iter() {
+		let start = Instant::now();
 
-            let primary = (beat_rolling * 25.0) as u8;
-            let secondary = primary / 2;
+		let base_volume = frame.info(|info| {
+			if info.beat != last_beat_num {
+				last_beat_num = info.beat;
+			}
 
-            // Alternate zone 1 and 2 colors on beat
-            let m = (last_beat_num & 1) as u8;
-            let n = m.bitxor(1);
-            
-            #[rustfmt::skip]
-            keyboard.set_colors_to(
-                &[
-                    0          , secondary  , primary, 
-                    m * primary, n * primary, 0      , 
-                    n * primary, m * primary, 0      , 
-                    0          , secondary  , primary, 
-                ]
-            );
+			info.beat_volume
+		});
 
-            info.beat_volume
-        });
+		beat_rolling = (beat_rolling * 0.95f32).max(base_volume);
 
-        beat_rolling = (beat_rolling * 0.95f32).max(base_volume);
+		let primary = (beat_rolling * 32.0) as u8;
+		let secondary = primary / 2;
 
-        let end = std::time::Instant::now();
-        let dur = end - start;
-        if dur < frame_time {
-            let sleep = frame_time - dur;
-            std::thread::sleep(sleep);
-        }
-    }
+		// Alternate zone 1 and 2 colors on beat
+		let m = (last_beat_num & 1) as u8;
+		let n = m.bitxor(1);
+
+		keyboard.set_colors_to(
+			#[rustfmt::skip]
+            &[
+                0          , secondary  , primary,
+                m * primary, n * primary, 0      ,
+                n * primary, m * primary, 0      ,
+                0          , secondary  , primary,
+            ],
+		);
+
+		frame_time
+			.checked_sub(start.elapsed())
+			.map(std::thread::sleep);
+	}
 }
