72b98bb36d1b4ffed34093498b62c37bedfe33ba
gtmnayan <gtmnayan@gmail.com>
2022-08-06 14:50:33 +0545

    wip

--- src/vol.rs
+++ src/vol.rs
@@ -1,21 +1,22 @@
-use std::ops::Range;
+use std::ops::RangeInclusive;
 
 use windows::Win32::{
 	Media::Audio::{
-		eConsole, eRender, Endpoints::IAudioEndpointVolume, IMMDeviceEnumerator, MMDeviceEnumerator,
+		eConsole, eRender,
+		Endpoints::{IAudioEndpointVolume, IAudioEndpointVolumeCallback},
+		IMMDevice, IMMDeviceEnumerator, MMDeviceEnumerator, AUDIO_VOLUME_NOTIFICATION_DATA,
 	},
-	System::Com::{CoCreateInstance, CoInitialize, CLSCTX_INPROC_SERVER},
+	System::Com::{CoCreateInstance, CoInitialize, CoUninitialize, CLSCTX_INPROC_SERVER},
 };
 
-pub struct Volume {
+pub struct EndpointWrapper {
+	volume: Volume,
 	endpoint: IAudioEndpointVolume,
 }
 
-const VOL_RANGE: Range<f32> = f32::MIN_POSITIVE..1.0;
-
-impl Volume {
+impl EndpointWrapper {
 	pub fn new() -> windows::core::Result<Self> {
-		let endpoint = unsafe {
+		let endpoint: IAudioEndpointVolume = unsafe {
 			CoInitialize(std::ptr::null())?;
 
 			CoCreateInstance::<_, IMMDeviceEnumerator>(
@@ -26,13 +27,34 @@ impl Volume {
 			.GetDefaultAudioEndpoint(eRender, eConsole)?
 			.Activate(CLSCTX_INPROC_SERVER, std::ptr::null())?
 		};
-		Ok(Self { endpoint })
+
+		// let cb: IAudioEndpointVolumeCallback =
+		// 	unsafe { device.Activate(CLSCTX_INPROC_SERVER, std::ptr::null())? };
+
+		Ok(Self {
+			volume: Volume::new(&endpoint),
+			endpoint,
+		})
 	}
 
 	pub fn get_intensity(&self, beat_volume: f32) -> u8 {
-		match unsafe { self.endpoint.GetMasterVolumeLevelScalar() } {
-			Ok(v) if VOL_RANGE.contains(&v) => (beat_volume * 20.0 / v) as u8,
-			_ => 0,
-		}
+		(self.volume.0 * beat_volume) as u8
 	}
+}
+
+const VOL_RANGE: RangeInclusive<f32> = f32::MIN_POSITIVE..=1.0;
+
+struct Volume(f32);
+
+impl Volume {
+	pub fn new(endpoint: &IAudioEndpointVolume) -> Self {
+		let new = Volume(match unsafe { endpoint.GetMasterVolumeLevelScalar() } {
+			Ok(v) if VOL_RANGE.contains(&v) => 20.0 / v,
+			_ => 0.0,
+		});
+
+		new
+	}
+
+	
 }
